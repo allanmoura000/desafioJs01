12985c1199e65610afd42e850b9116915cc5263b
Allan Moura <116190027+allanmoura000@users.noreply.github.com>
2023-05-19 09:04:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function showSla() {
+    sla.style.display = 'block'
+}
+
+
+let n1 = 5
+let n2 = 6
+s = n1 + n2
+
+
+const myHeading = document.querySelector("p");
+myHeading.textContent = "A soma é: " + s;
+
+function showSoma(){
+    soma.style.display = 'block'
+}
